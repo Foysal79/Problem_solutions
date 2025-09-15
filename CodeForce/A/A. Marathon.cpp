e9992e2640e16ecf813b77f6f0fdd414ef2e3645
Foysal79 <onthor312001@gmail.com>
2025-09-15 10:44:03 +0600

    a maratyhon

---
+++ CodeForce/A/A. Marathon.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin >> t;
+    int count = 0;
+    while (t--)
+    {
+        int count = 0;
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        if (a < b)
+        {
+            count += 1;
+        }
+        if (a < c)
+        {
+            count += 1;
+        }
+        if (a < d)
+        {
+            count += 1;
+        }
+        cout << count << endl;
+    }
+
+    return 0;
+}
